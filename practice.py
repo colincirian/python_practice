19e1ade5ce0274089fbc55760d645ad111608329
Colin <colindcirian@gmail.com>
2024-02-13 20:33:50 -0800

    math and random

--- practice.py
+++ practice.py
@@ -1,3 +1,6 @@
+import math
+import random
+
 a = 0
 b = 5
 c= 2
@@ -8,4 +11,15 @@ print("Colin " + "Cirian") #CONCATENATION
 
 print(b/c)
 
+print(abs(-20))
+print(math.sqrt(64))
+print(pow(9, 8))
+print(random.random())
+print(random.randrange(1, 10))
+
+def abs_diff(num1, num2):
+    return abs(num1 - num2)
+
+result = abs_diff(-14, -9)
+print(f"The absolute difference between -14 and -9 is: {result}")
 
